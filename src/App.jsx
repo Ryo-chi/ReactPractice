cc48832bb320cd39a255015d05b470626041c5d4
Ryo <88888729+Ryo-chi@users.noreply.github.com>
2022-12-28 22:08:35 +0900

    usestateの学習

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFrag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFrag(!faceShowFlag);
   };
 
   return (
@@ -11,8 +18,11 @@ const App = () => {
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue" message="お元気ですか" />
       <ColorfulMessage color="pink" message="元気です" />
-
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(≧▽≦)</p>}
     </>
   );
 };
