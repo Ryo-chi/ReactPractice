8da94ea74ba8fb53f6227c74e460b141c1a71add
Ryo <88888729+Ryo-chi@users.noreply.github.com>
2022-12-28 20:59:50 +0900

    propsの使い方1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorfulMessage color="blue" message="お元気ですか" />
+      <ColorfulMessage color="pink" message="元気です" />
+
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
